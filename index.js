95f2726f9c9438041d7b9313eba8e1eb76a90f68
Mark Gowen <mark.gowen78@gmail.com>
2022-11-02 08:15:58 -0700

    commit tree 6c5453ff882f750c08d05dfcc31fd793c82859d8

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return (`This Saturday, I want to ${activity}!`);
+}
+
+function mondayWork(activity = "go to the office") {
+    return (`This Monday, I will ${activity}.`);
+}
+
+let wrapAdjective = function(flair = "*") {
+   return function(innerFunction = "special") {
+    return `You are ${flair}${innerFunction}${flair}!`
+   }
+}
+
+// function outer(greeting, msg = "It's a fine day to learn") {
+//     // 2
+//     const innerFunction = function (name, lang = "Python") {
+//       // 3
+//       return `${greeting}, ${name}! ${msg} ${lang}`; // 4
+//     };
+//     return innerFunction("student", "JavaScript"); // 5
+//   }
+  
+//   outer("Hello");
